995844eddbcd2e7a8841c4e59ea9bdb82ca64462
vikasrushi <0xvikas@gmail.com>
2022-09-05 01:52:52 +0530

    Kadane's Algorithms

---
+++ Algorithms/src/Kadane_algo.java
@@ -0,0 +1,20 @@
+public class Kadane_algo {
+    public static void main(String[] args) {
+        int[] nums = {-1,-2,-3,-4};
+        System.out.println(maxSubArray(nums));
+    }
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i : nums){
+            sum+=i;
+            if(max<sum){
+                max =sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+}
